74d79412233d13c4461da4f8decc89e83d01d00f
MABNACARD\hsharifi <handroidsharify@gmail.com>
2020-09-15 15:07:33 +0430

    Update build.gradle Open gallery

--- app/src/main/java/ir/videoconverterlab/app/MainActivity.java
+++ app/src/main/java/ir/videoconverterlab/app/MainActivity.java
@@ -1,15 +1,19 @@
 package ir.videoconverterlab.app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
+import android.view.View;
 
 import com.CodeBoy.MediaFacer.MediaFacer;
 import com.CodeBoy.MediaFacer.VideoGet;
@@ -33,20 +37,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import ir.videoconverterlab.app.databinding.ActivityMainBinding;
+
 import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_CANCEL;
 import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_SUCCESS;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<videoContent> videoContents = new ArrayList<>();
+    ActivityMainBinding binding;
+
+    int SELECT_VIDEO_REQUEST = 100;
+    String selectedVideoPath;
+    String filemanagerstring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.openGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("video/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Video"), SELECT_VIDEO_REQUEST);
+            }
+        });
 
 
-        Dexter.withContext(this)
+        /*Dexter.withContext(this)
                 .withPermissions(
                         Manifest.permission.READ_EXTERNAL_STORAGE
                 ).withListener(new MultiplePermissionsListener() {
@@ -84,11 +107,7 @@ public class MainActivity extends AppCompatActivity {
             public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
 
             }
-        }).check();
-
-
-//        String extStore = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
-
+        }).check();*/
 
     }
 
@@ -106,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
 
         ContentValues valuesvideos;
         valuesvideos = new ContentValues();
-         valuesvideos.put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/" + "Folder");
+        valuesvideos.put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/" + "Folder");
         valuesvideos.put(MediaStore.Video.Media.TITLE, videoFileName);
         valuesvideos.put(MediaStore.Video.Media.DISPLAY_NAME, videoFileName);
         valuesvideos.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
@@ -145,8 +164,6 @@ public class MainActivity extends AppCompatActivity {
             pfd.close();
 
 
-
-
         } catch (Exception e) {
 
             e.printStackTrace();
@@ -156,5 +173,38 @@ public class MainActivity extends AppCompatActivity {
         valuesvideos.clear();
         valuesvideos.put(MediaStore.Video.Media.IS_PENDING, 0);
         this.getContentResolver().update(uriSavedVideo, valuesvideos, null, null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_VIDEO_REQUEST) {
+                Uri selectedImageUri = data.getData();
+
+                // OI FILE Manager
+                filemanagerstring = selectedImageUri.getPath();
+
+                // MEDIA GALLERY
+                selectedVideoPath = getPath(selectedImageUri);
+                if (selectedVideoPath != null) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public String getPath(Uri uri) {
+        String[] projection = {MediaStore.Video.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            // HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+            // THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else
+            return null;
     }
 }
